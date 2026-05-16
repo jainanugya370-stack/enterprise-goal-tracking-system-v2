eae5d5cb94d67421b60ca1338a953739508d8b11
Anugya Jain <Anugya.23fe10cai00402@muj.manipal.edu>
2026-05-17 00:57:37 +0530

    Implemented enterprise notification system

---
+++ notifications/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Notification
+
+
+def notification_list(request):
+
+    notifications = Notification.objects.filter(
+        user=request.user
+    ).order_by('-created_at')
+
+    return render(
+        request,
+        'notifications.html',
+        {
+            'notifications': notifications
+        }
+    )
