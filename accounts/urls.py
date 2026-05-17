4c9570b2ba1910f04d3d73570b592556db45fb0a
Anugya Jain <Anugya.23fe10cai00402@muj.manipal.edu>
2026-05-17 21:02:26 +0530

    Fixed login URL routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,29 @@ from .views import login_view, logout_view, create_demo_user
 
 urlpatterns = [
 
-    path('', login_view, name='login'),
+    # HOME LOGIN
+    path(
+        '',
+        login_view,
+        name='login'
+    ),
 
-    path('logout/', logout_view, name='logout'),
+    # EXPLICIT LOGIN URL
+    path(
+        'login/',
+        login_view,
+        name='login_page'
+    ),
 
-    path('create-demo-user/', create_demo_user),
+    path(
+        'logout/',
+        logout_view,
+        name='logout'
+    ),
 
+    path(
+        'create-demo-user/',
+        create_demo_user,
+        name='create_demo_user'
+    ),
 ]
